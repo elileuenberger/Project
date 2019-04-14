35e7adae881a109d9922948674b2574062d05ccb
mblahnik <mblahnik@uwm.edu>
2019-04-14 18:03:18 -0500

    bncvcbnvnbvnbv

--- Account/views.py
+++ Account/views.py
@@ -29,3 +29,33 @@ class taPage(View):
         CUH = CurrentUserHelper()
         Account = CUH.getCurrentUser()
         return render(request, 'Accounts/TaHome.html', {"Account": Account})
+
+class createAccount(View):
+
+    def get(self, request):
+        return render(request, 'createAccount.html')
+
+    def post(self, request):
+
+        userName = str(request.POST["username"])
+        firstName = str(request.POST["firstname"])
+        lastName = str(request.POST["lastname"])
+        password = str(request.POST["password"])
+        email = str(request.POST["email"])
+        title = str(request.POST["title"])
+        address = str(request.POST["address"])
+        city = str(request.POST["city"])
+        state = str(request.POST["state"])
+        zipCode = str(request.POST["zipcode"])
+        officeNumber = str(request.POST["officenumber"])
+        officePhone = str(request.POST["officephone"])
+        officeDays = str(request.POST["officeday"])
+        officeHoursStart = str(request.POST["officehourstart"])
+        officeHoursEnd = str(request.POST["officehoursend"])
+        currentUser = str(request.POST["dfshjkhljfds"])
+
+        try:
+            message = command = ["", userName, title, email, firstName, lastName, password, address, city, state, zipCode, officeNumber, officePhone, officeDays, officeHoursStart, officeHoursEnd]
+            return render(request, 'loginscreen.html', {"message": message})
+        except Exception as e:
+            return render(request, 'loginscreen.html', {"message": str(e)})
