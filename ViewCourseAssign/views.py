27e9bbc5ef95fc1f0c7c0d0956dbc6952893d3ae
mblahnik <mblahnik@uwm.edu>
2019-04-15 13:52:08 -0500

    bncvcbnvnbvnbv

--- ViewCourseAssign/views.py
+++ ViewCourseAssign/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.views import View
+from Account.models import Account
+from ViewCourseAssign.models import viewCourseAssign
 # Create your views here.
+
+
+class viewAssign(View):
+
+    def get(self, request):
+        taList = Account.objects.filter(title=1)
+        return render(request, 'viewAssignments.html', {"taList": taList})
+
+    def post(self, request):
+        taList = Account.objects.filter(title=1)
+        account = str(request.POST["taAccount"])
+
+        command = ["", account]
+
+        vca = viewCourseAssign()
+
+        message = vca.viewCourseAssign(command)
+
+        return render(request, 'viewAssignments.html', {"assignments": message, "taList": taList})
