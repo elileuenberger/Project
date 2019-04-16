6ff8f3621fa88097ae56d13aa76d995f9dcfe45f
Erik <spykim2003@gmail.com>
2019-04-15 23:22:48 -0500

    Created the django unit tet for deleteaccount command

--- Tests/UnitTests/Test_DeleteAccount.py
+++ Tests/UnitTests/Test_DeleteAccount.py
@@ -3,3 +3,8 @@ from django.test import TestCase
 from Account.CreateAccount import CreateAccount
 from Account.DeleteAccount import DeleteAccount
 from Account.models import Account
+
+
+class Test_DeleteAccount(TestCase):
+    def setUp(self):
+        pass
